6526c205fee456d280539b92686c24af057c9a40
puwka <epicgames4438@gmail.com>
2026-05-12 13:57:34 +0300

    пмлсоргмлрпгол

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  outputFileTracingExcludes: {
+    "/*": [
+      "./public/uploads/generations/**/*",
+      "./public/uploads/voice-previews/**/*",
+    ],
+  },
+};
 
 export default nextConfig;
